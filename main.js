44dbc9426b7751143974d1662291b89e1c8ceccf
AdityaSharma42 <adityasharma3@usf.edu>
2024-11-03 00:57:43 -0400

    Dynamically display tickets on the page.

--- main.js
+++ main.js
@@ -1,23 +1,41 @@
 async function fetchTickets() {
-    const API_URL= 'https://jsonplaceholder.typicode.com/posts';
-    const tixContainer=document.getElementById('ticket-container');
+    const ticketContainer=document.getElementById('ticket-container');
     const errorMessage=document.getElementById('error-message');
 
 try{
-    const response= await fetch(API_URL);
+    const response= await fetch('https://jsonplaceholder.typicode.com/posts');
     if (!response.ok){
         throw new Error('Network response issue');
     }
 
-const data = await response.json();
-if (data.length===0){
+const tickets = await response.json();
+if (tickets.length===0){
     throw new Error ('No tickets currently available');
 }
 
-displayTickets(data);
+displayTickets(tickets);
 }catch (error){
     errorMessage.textContent= error.message;
 }
 
 }
+function displayTickets(tickets){
+    const ticketContainer= document.getElementById('ticket-container');
+
+    tickets.forEach(ticket => {
+        const ticketDiv= document.createElement('div');
+        ticketDiv.innerHTML=`
+        <h2> Ticket ID: ${ticket.id}</h2>
+        <p> Customer Name: User ${ticket.userId}</p>
+        <p> Issue Descriptions: ${ticket.title}</p>
+        <p> Details:${ticket.body}</p>
+        `;
+
+        ticketContainer.appendChild(ticketDiv);
+        
+    });
+}
+fetchTickets();
+
+
 

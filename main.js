0816894cf8ab03179e525045eaac41fbb2722282
AdityaSharma42 <adityasharma3@usf.edu>
2024-11-03 01:43:44 -0400

    Use finally to clean up after the fetch.

--- main.js
+++ main.js
@@ -1,41 +1,44 @@
-async function fetchTickets() {
+async function fetchTickets() { //async function to fetch tickets from the API
     const ticketContainer=document.getElementById('ticket-container');
     const errorMessage=document.getElementById('error-message');
+    const loadingMessage=document.getElementById('loading');
+
+loadingMessage.style.display='block';
 
 try{
-    const response= await fetch('https://jsonplaceholder.typicode.com/posts');
+    const response= await fetch('https://jsonplaceholder.typicode.com/posts'); //fetching data from the API
     if (!response.ok){
-        throw new Error('Network response issue');
+        throw new Error('Network response issue');//throws an error
     }
 
 const tickets = await response.json();
-if (tickets.length===0){
-    throw new Error ('No tickets currently available');
+if (tickets.length===0){//checking if the tickets array is empty
+    throw new Error ('No tickets currently available'); //throws an error
 }
 
-displayTickets(tickets);
+displayTickets(tickets); //calling the function, this displays the fetched tickets
+
 }catch (error){
-    errorMessage.textContent= error.message;
+    errorMessage.textContent= error.message; //displays errors that may have occurred
+} finally{
+    loadingMessage.style.display='none';//hides the loading message
 }
 
 }
 function displayTickets(tickets){
     const ticketContainer= document.getElementById('ticket-container');
 
-    tickets.forEach(ticket => {
-        const ticketDiv= document.createElement('div');
+    tickets.forEach(ticket => { //go through every ticket in the array
+        const ticketDiv= document.createElement('div'); //holds ticket information
         ticketDiv.innerHTML=`
         <h2> Ticket ID: ${ticket.id}</h2>
         <p> Customer Name: User ${ticket.userId}</p>
         <p> Issue Descriptions: ${ticket.title}</p>
-        <p> Details:${ticket.body}</p>
-        `;
+        <p> Details:${ticket.body}</p> 
+        `; 
 
-        ticketContainer.appendChild(ticketDiv);
+        ticketContainer.appendChild(ticketDiv); 
         
     });
 }
-fetchTickets();
-
-
-
+fetchTickets(); //calling the function
